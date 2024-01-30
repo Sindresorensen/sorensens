6ab567bbf7b298001918ffb09593572038d3636c
Sindresorensen <sindre71@hotmail.com>
2024-01-30 19:50:52 +0100

    nailed it!

--- src/components/general/Navbar.tsx
+++ src/components/general/Navbar.tsx
@@ -94,6 +94,8 @@ export function Navbar() {
     const { isNynorsk } = useContext(LanguageContext);
     
     const [hidden, setHidden] = useState(false);
+    const [showMenu, setShowMenu] = useState(true);
+    let prevScrollPos = 0;
 
     const handleScroll = () => {
         const fromTop = Math.abs(document.body.getBoundingClientRect().top);
@@ -102,6 +104,15 @@ export function Navbar() {
         } else {
             setHidden(false);
         }
+        
+        if ((fromTop > 100 && fromTop - prevScrollPos < 0) || fromTop < 100) {
+            setShowMenu(true);
+        } else {
+            setShowMenu(false);
+        }
+
+        prevScrollPos = fromTop;
+
     };
 
     useEffect(() => {
@@ -116,7 +127,10 @@ export function Navbar() {
     return (
         <>
 
-        <div className="flex sm:hidden absolute z-30 w-full">
+        <div className={cn(
+            "flex sm:hidden fixed z-30 w-full transition duration-500", 
+            (showMenu ? "opacity-100" : "opacity-0"))}
+        >
             <div className="flex items-center justify-between w-full mt-6 mx-6">
 
                 <div className="flex sm:hidden h-10 items-center justify-between">
@@ -128,7 +142,10 @@ export function Navbar() {
         </div>
 
 
-        <div className={cn("hidden sm:flex justify-between fixed pt-8 px-8 pb-2 z-30 w-full transition duration-500 sm:hover:opacity-100", (hidden ? "sm:opacity-0" : "sm:opacity-100"))}>
+        <div className={cn(
+            "hidden sm:flex justify-between fixed pt-8 px-8 pb-2 z-30 w-full transition duration-500 sm:hover:opacity-100", 
+            (hidden ? "sm:opacity-0" : "sm:opacity-100"))}
+        >
             <div className=" flex items-center justify-left gap-6 ">
                 <Link to="/">
                     <NavButton color="white" className="uppercase font-extralight">{isNynorsk ? "Heim" : "Hjem"}</NavButton>
