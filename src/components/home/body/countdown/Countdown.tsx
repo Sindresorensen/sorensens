411ae1c69bcdc36764f2662372042380ec87ac04
Sindresorensen <sindre71@hotmail.com>
2024-03-29 20:21:04 +0100

    Liten bøgg

--- src/components/home/body/countdown/Countdown.tsx
+++ src/components/home/body/countdown/Countdown.tsx
@@ -26,7 +26,7 @@ function Countdown() {
 
                 <div className=" m-10 mx-4 sm:mx-10 flex items-center gap-4 sm:gap-10 justify-center">
                     {weeks > 0 && <CountdownSection timeLeft={weeks} caption={isNynorsk ? (weeks === 1 ? "veke" : "veker") : (weeks === 1 ? "uke" : "uker")} />}
-                    {days > 0 && <CountdownSection timeLeft={days} caption={isNynorsk ? (days === 1 ? "dag" : "dagar") : (days === 1 ? "dag" : "dager")} />}
+                    {(days > 0 || weeks !== 0) && <CountdownSection timeLeft={days} caption={isNynorsk ? (days === 1 ? "dag" : "dagar") : (days === 1 ? "dag" : "dager")} />}
                     <CountdownSection timeLeft={hours} caption={isNynorsk ? (hours === 1 ? "time" : "timar") : (hours === 1 ? "time" : "timer")} />
                     {weeks === 0 && <CountdownSection timeLeft={minutes} caption={isNynorsk ? (minutes === 1 ? "minutt" : "minutt") : (minutes === 1 ? "minutt" : "minutter")} />}
                     {(weeks === 0 && days === 0) && <CountdownSection timeLeft={seconds} caption={isNynorsk ? (seconds === 1 ? "sekund" : "sekund") : (seconds === 1 ? "sekund" : "sekunder")} />}
