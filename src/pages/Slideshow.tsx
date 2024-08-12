e3e884e206ee2cc36827085e9a1f1e3cc060dcf1
Sindresorensen <sindre71@hotmail.com>
2024-08-12 21:25:26 +0200

    Oppdatert måten man henter inn bilder

--- src/pages/Slideshow.tsx
+++ src/pages/Slideshow.tsx
@@ -1,5 +1,5 @@
-import { useEffect, useRef, useState } from "react";
-import { Button } from "@/components/ui/button";
+import { ChangeEvent, useState } from "react";
+import { Button, buttonVariants } from "@/components/ui/button";
 import { XMarkIcon } from "@heroicons/react/24/outline";
 import Slide from "@/components/slideshow/Slide";
 import { cn } from "@/lib/utils";
@@ -7,38 +7,27 @@ import { Label } from "@/components/ui/label";
 import DurationSelector from "@/components/slideshow/DurationSelector";
 import TransitionSelector from "@/components/slideshow/TransitionSelector";
 
-const filteredObjects = (files: File[]): File[] => {
-    return files.filter( file => file.type.includes("image") );
-}
-
 export const secs = Array.from({ length: 30 }, (_, i) => i + 1);
 
 export default function Slideshow() {
 
-    const fileInputRef = useRef<HTMLInputElement>(null);
-    const [files, setFiles] = useState<string[]>([]);
     const [seconds, setSeconds] = useState(10);
     const [hasTransition, setHasTransition] = useState(false);
-
+    const [images, setImages] = useState<string[]>([]);
     const [isShowingSlide, setIsShowingSlide] = useState(false);
 
-    useEffect(() => {
-        if (fileInputRef && fileInputRef.current) {
-            fileInputRef.current.setAttribute('webkitdirectory', 'true');
-            fileInputRef.current.setAttribute('directory', 'true');
+    const sjekkFiler = (e: ChangeEvent<HTMLInputElement>) => {
+        const files = e.target.files;
+
+        if (files) {
+            const imageFiles = Array.from(files).filter(file =>
+                /\.(jpg|jpeg|png|gif|webp)$/.test(file.name.toLowerCase())
+            );
+
+            const imageUrls = imageFiles.map(file => URL.createObjectURL(file));
+            setImages(imageUrls);
+            console.log(imageFiles)
         }
-    }, []);
-
-    const handleChange = (e: React.ChangeEvent<HTMLInputElement>) => {
-        const uploadedFiles = Array.from(e.target.files || []);
-        const filteredFiles = filteredObjects(uploadedFiles);
-
-        const objectURLS = filteredFiles.map(file => URL.createObjectURL(file) )
-        setFiles(objectURLS);
-    }
-
-    const onChooseImageClick = () => {
-        fileInputRef.current?.click();
     }
 
     return (
@@ -47,13 +36,14 @@ export default function Slideshow() {
                 <div className=" flex flex-col items-center justify-center gap-12">
                     <div className="flex items-center gap-1.5">
                         <div>
-                            <Button onClick={onChooseImageClick}>Velg mappe</Button>
-                            <input className="hidden" type="file" id="fileInput" ref={fileInputRef} onChange={handleChange}/>
+                            <input className="hidden" id="imageInput" type="file" multiple accept="image/*" onChange={sjekkFiler} />
+                            {/* Label som bruker knapp-style */}
+                            <label htmlFor="imageInput" className={cn(buttonVariants(), "cursor-pointer")}>Velg mappe</label>
                         </div>
-                        {files.length === 0 && <p className="text-sm">Ingen filer valgt</p>}
-                        {files.length > 0 && <p className="text-sm">{files.length} filer valgt</p>}
+                        {images.length === 0 && <p className="text-sm">Ingen filer valgt</p>}
+                        {images.length > 0 && <p className="text-sm">{images.length} filer valgt</p>}
 
-                        <div className={cn("ml-6 flex items-center group cursor-pointer", files.length === 0 && "hidden")} onClick={() => setFiles([])}>
+                        <div className={cn("ml-6 flex items-center group cursor-pointer", images.length === 0 && "hidden")} onClick={() => setImages([])}>
                             <XMarkIcon className="h-6 " />
                             <p className="text-slate-500 text-xs opacity-0 group-hover:opacity-100 transition">Fjern</p>
                         </div>
@@ -77,14 +67,14 @@ export default function Slideshow() {
 
                     <Button variant="outline" 
                         className="border-black px-10 font-medium hover:bg-primary hover:text-primary-foreground" 
-                        disabled={files.length === 0}
+                        disabled={images.length === 0}
                         onClick={() => setIsShowingSlide(true)}
                     >Start
                     </Button>
                 </div>
             </div>
 
-            {isShowingSlide && <Slide images={files} 
+            {isShowingSlide && <Slide images={images} 
                 seconds={seconds} 
                 setSeconds={setSeconds} 
                 hasTransition={hasTransition}
